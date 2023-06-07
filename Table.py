1dfcaba74aa8d35c7c21a46eaab9b591316292b5
UsernameNotAvalable <89629084+UsernameNotAvalable@users.noreply.github.com>
2023-06-07 15:48:21 +1000

    Update Table.py

--- Table.py
+++ Table.py
@@ -2,8 +2,9 @@
 def Checker():
    user_input = input(": ")
    while user_input.isdigit() == False:
-       user_input = input("Please put a number: ")
+       user_input = input("Please put a positive number: ")
    return user_input
+
 
 #placed for double-click usage of code, keeps the terminal open for reading of multiplication table.
 def Finished():
@@ -11,24 +12,38 @@ def Finished():
 
 
 print("Welcome! Please use the multiplication table whenever needed! Enjoy.")
-print("Give me a multiplicand")
+print("Give me a multiplicand in numerical form.")
 #specified int check so there are no issues with string inputs in math logic.
-Multiplicand = int(Checker())
+Multiplicand = (Checker())
 print("You have given", Multiplicand)
 
 
-print("Give me a multiplier")
-Multiplier = int(Checker())
+print("Give me a multiplier in numerical form.")
+Multiplier = (Checker())
 print("You have given", Multiplier)
 
-
 print("Give me a range / times of multiplication")
-Times = int(Checker())
+Times = (Checker())
+
+
+def CheckerWithoutInput(Times):
+   user_input = Times()
+   while user_input.isdigit() == False:
+       user_input = input("Please put a positive number: ")
+   return user_input
+
+while Times < Multiplicand:
+    Times = input("Please make sure your range or Times is higher than your Multiplicand.")
+    Times = CheckerWithoutInput()
+
 print("You have given", Times)
 
 #printing each choice integer for verification.
 print("Your multiplicand is: ", Multiplicand, "| ", "Your multiplier is: ", Multiplier, "| ", "Your times is: ", Times, "|")
 
+Multiplicand = int(Multiplicand)
+Multiplier = int(Multiplier)
+Times = int(Times)
 
 Product = Multiplicand * Multiplier
 #in range times - Start and end ranges for the math logic.
